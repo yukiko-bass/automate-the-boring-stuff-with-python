a2ecfc69f52cd84571e05d621f6e630a8dbdabc7
kato_y <kato_y@auroraworks.co.jp>
2019-10-27 17:08:22 +0900

    2章 フロー制御

---
+++ Chapter2/2-7-3_if/littleKid.py
@@ -0,0 +1,8 @@
+name = input()
+age = int(input())
+if name == 'Alice':
+    print('やあ、Alice。')
+elif age < 12:
+    print('Aliceじゃないね、お嬢ちゃん。')
+else:
+    print('君はAliceでも子供でもないね。')
---
+++ Chapter2/2-7-3_if/vampire.py
@@ -0,0 +1,10 @@
+name = input()
+age = int(input())
+if name == 'Alice':
+    print('やあ、Alice。')
+elif age < 12:
+    print('Aliceじゃないね、お嬢ちゃん。')
+elif age > 2000:
+    print('Aliceはお前のような不死身ではない、吸血鬼め。')
+elif age > 100:
+    print('Aliceじゃないね、おばあちゃん。')
