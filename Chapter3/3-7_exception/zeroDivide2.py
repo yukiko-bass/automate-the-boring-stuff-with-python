f27f458844e93eade07c6f082026114f6134414b
kato_y <kato_y@auroraworks.co.jp>
2020-06-06 16:33:15 +0900

    3章 関数

---
+++ Chapter3/3-7_exception/zeroDivide2.py
@@ -0,0 +1,11 @@
+def spam(divide_by):
+    return 42 / divide_by
+
+
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+except ZeroDivisionError:
+    print('エラー:不正な引数です。')
